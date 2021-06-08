2d63667389257cb5f2c1faee29f8d1de0c6fe28f
blabla-my <1042244638@qq.com>
2021-06-08 05:29:48 -0600

    sequence and concurrency version ; no cache

--- proxy.c
+++ proxy.c
@@ -10,14 +10,13 @@ static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64;
 /* declaration */
 
 /* proxy */
-int forward(char * packet, size_t length, struct sockaddr dest);
 int parseHttpRequest(char * httpReq, size_t length, char* parseResult);
-void proxy_doit(int* fd);
+void *proxy_doit(void* fd);
 int parse_uri(char * uri, char * host, char * path, int * port);
 
 int main(int argc, char* argv[])
 {
-    int listenfd, connfd;
+    int listenfd;
     char hostname[MAXLINE], port[MAXLINE];
     socklen_t clientlen;
     struct sockaddr_storage clientaddr;
@@ -31,29 +30,34 @@ int main(int argc, char* argv[])
     listenfd = Open_listenfd(argv[1]);
     while (1) {
         clientlen = sizeof(clientaddr);
-        connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen); 
+        int * connfdp = (int *)malloc(sizeof(int));
+        *connfdp = Accept(listenfd, (SA *)&clientaddr, &clientlen); 
             Getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE, 
                         port, MAXLINE, 0);
             printf("Accepted connection from (%s, %s)\n", hostname, port);
-        proxy_doit(&connfd);                                           
-        Close(connfd);                                            
+        /* create the thread */
+        pthread_t tid;
+        Pthread_create(&tid, NULL, proxy_doit, connfdp);                                         
     }
     return 0;
 }
 
-void proxy_doit(int * pfd){
+void *proxy_doit(void * pfd){
+    /* detach the thread itself */
+    Pthread_detach(Pthread_self());
+
     char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
     rio_t rio;
-    int fd = *pfd;
+    int fd = *(int *)pfd;
 
     Rio_readinitb(&rio, fd);
     if (!Rio_readlineb(&rio, buf, MAXLINE)){
-        return;
+        return NULL;
     }
     sscanf(buf, "%s %s %s", method, uri, version);
     if (strcmp(method, "GET")) {
         //clienterror(fd, method, "501", "Not Implemented", "Not Implemented");
-        return ;
+        return NULL;
     }
     char host[MAXLINE], path[MAXLINE];
     int port;
@@ -61,7 +65,7 @@ void proxy_doit(int * pfd){
     /* parse the request line */
     int r;
     if((r = parse_uri(uri,host,path,&port)) == -1){
-        return ;
+        return NULL;
     }
     char cport[16];
     sprintf(cport, "%d", port);
@@ -72,7 +76,7 @@ void proxy_doit(int * pfd){
     int sockfd = open_clientfd(host,cport);
     if(sockfd == -1){
         close(sockfd);
-        return;
+        return NULL;
     }
     /* request head line */
     sprintf(buf, "%s %s HTTP/1.0\r\n", method, path);
@@ -99,14 +103,17 @@ void proxy_doit(int * pfd){
     Rio_writen(sockfd, "\r\n", 2);
 
     /* recv from server */
-    rio_t client_rio;
-    Rio_readinitb(&client_rio, sockfd);
     char * resp = calloc(MAX_OBJECT_SIZE, sizeof(char));
     size_t n = Rio_readn(sockfd, resp, MAX_OBJECT_SIZE);
     
     /* return the object to the client */
     Rio_writen(fd, resp, n);
     /* update the cache with the new object */
+
+    /* close the socket */
+    Close(sockfd);
+    Close(fd);
+    return NULL;
 }
 
 int parse_uri(char * uri, char * host, char * path, int * port){
